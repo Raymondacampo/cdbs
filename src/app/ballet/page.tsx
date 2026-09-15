c97071c371dbfb707809210d16ca24c2c04ffc56
Raymond A’ campo Sandoval <raymondacamposandoval@Raymonds-MacBook-Air.local>
2026-09-15 13:34:13 -0400

    sorangel fixes & image position correction

--- src/app/ballet/page.tsx
+++ src/app/ballet/page.tsx
@@ -15,7 +15,7 @@ export default function BalletPage() {
         phone="text-white max-w-[80%] mt-[4dvh] md:mt-0 mr-auto"
         desktop="md:text-white md:max-w-[65%] md:top-8 md:mr-8"
         img={{ src: movCreativo, width: "w-[60dvw] h-auto sm:w-[50dvw] md:w-[40dvw] md:max-w-[330px] lg:max-w-none lg:w-auto lg:h-[50dvh] ", 
-        position: "absolute -right-[25%] bottom-0 sm:-right-[15%] md:bottom-1 md:-rotate-1 md:right-0 lg:relative lg:translate-y-[5.3%] xl:bottom-2 xl:translate-y-[8%]" }}
+        position: "absolute -right-[25%] bottom-0 sm:-right-[15%] md:bottom-1 md:-rotate-1 md:right-0 lg:relative lg:translate-y-[8.3%] xl:translate-y-[8%]" }}
         bannercolor="bg-peach"
         bg="bg-pink"
         dayTime={{ dayTimes: [
@@ -23,7 +23,7 @@ export default function BalletPage() {
           { day: "Martes y Jueves", time: "3:00pm - 4:00pm" },
           { day: "Sabados", time: "10:30am - 12:00pm" }
         ] }}
-        age={{ age: "Menor de 8 años", icon: <span>👶</span> }}
+        age={{ age: "2.5 - 5 años", icon: <span>👶</span> }}
         order="first"
         description="Estimula coordinación, equilibrio y gracia a través del juego, la música y la creatividad."
         variant="right"
@@ -39,10 +39,10 @@ export default function BalletPage() {
         bannercolor="bg-peach"
         bg="bg-pink"
         dayTime={{ dayTimes: [
-          { day: "Lunes y Miercoles", time: "12:00pm - 1:00pm" },
+          { day: "Lunes y Miercoles", time: "3:30pm - 4:30pm" },
           { day: "Martes", time: "2:00pm - 3:00pm" },
         ] }}
-        age={{ age: "3 - 8 años", icon: <span>👶</span> }}
+        age={{ age: "6 - 7 años", icon: <span>👶</span> }}
         order="second"
         description="Introduce a niños pequeños a la danza mediante juegos, música y ejercicios creativos que estimulan su coordinación y ritmo."
         variant="left"
@@ -54,7 +54,8 @@ export default function BalletPage() {
       <Class
         phone="text-white max-w-[80%] mt-[4dvh] xl:mt-8"
         desktop="md:text-white md:max-w-[65%] md:top-4 md:mr-8"
-        img={{ src: balletI, width: "w-[50dvw] h-auto max-w-[280px] md:w-[30dvh] lg:h-[49dvh] lg:w-auto xl:h-[50.5dvh]", position: "absolute -right-[18%] sm:-right-[10%] bottom-0 md:-bottom-2 md:-rotate-1 md:right-[5%] md:bottom-1 lg:-left-8 lg:-bottom-6.5 lg:relative" }}
+        img={{ src: balletI, width: "w-[50dvw] h-auto max-w-[280px] md:w-[30dvh] lg:h-[49dvh] lg:w-auto xl:h-[50.5dvh]", 
+        position: "absolute -right-[18%] sm:-right-[10%] bottom-0 md:-bottom-2 md:-rotate-1 md:right-[5%] md:bottom-1 lg:-left-8 lg:translate-y-[10.8%] lg:relative" }}
         bannercolor="bg-peach"
         bg="bg-pink"
         dayTime={{ dayTimes: [
@@ -78,7 +79,7 @@ export default function BalletPage() {
           bannercolor="bg-peach"
           bg="bg-pink"
           dayTime={{ dayTimes: [
-            { day: "Martes y Jueves", time: "3:30pm - 4:30pm" },
+            { day: "Martes y Jueves", time: "3:30pm - 5:00pm" },
           ] }}
           age={{ age: "12 - 17 años", icon: <span>👶</span> }}
           order="second"
@@ -97,7 +98,7 @@ export default function BalletPage() {
           bannercolor="bg-peach"
           bg="bg-pink"
           dayTime={{ dayTimes: [
-            { day: "Martes y Jueves", time: "4:00pm - 5:30pm" },
+            { day: "Martes y Jueves", time: "4:30pm - 6:00pm" },
           ] }}
           age={{ age: "12 en adelante", icon: <span>👶</span> }}
           order="first"
